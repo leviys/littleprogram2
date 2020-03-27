f6694294100ac47c29867e664c87a61f58fd1539
leviys <leviys@163.com>
2020-03-27 15:03:30 +0800

    Motify2020/03/27

--- pages/veido/veido.js
+++ pages/veido/veido.js
@@ -1,18 +1,35 @@
-// pages/veido/veido.js
+
+const list = require('../../utils/data.js')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    list:[],
+    msg:'Nihao',
+    ad:'/images/shi3.mp4'
+  },
+   
+  videoErrorCallback: function (e) {
+    console.log('视频错误信息:' + e.detail.errMsg);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      list:list.data
+    })
+    console.log(this.data.list)
+    // wx.request({
+    //   url: 'https://api.budejie.com/api/api_open.php?a=list&c=data&type=41',
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
   },
 
   /**
---
+++ utils/data.js
@@ -0,0 +1,46 @@
+// const shi1 = require('../images/ship2.mp4')
+// const shi2 = require('../../images/shi2.mp4')
+// const shi3 = require('../../images/shi3.mp4')
+// const shi4 = require('../../images/shi4.mp4')
+// const shi5 = require('../../images/shi5.mp4')   //报错
+
+let data = [
+  {
+    zan: 110,
+    buzan: 20,
+    zhuan:6,
+    liuyan:40,
+    address:"http://media.w3.org/2010/05/sintel/trailer.mp4"
+  },
+  {
+    zan: 110,
+    buzan: 20,
+    zhuan: 6,
+    liuyan: 40,
+    address: "/images/shi2.mp4"
+  },
+  {
+    zan: 110,
+    buzan: 20,
+    zhuan: 6,
+    liuyan: 40,
+    address: "/images/shi3.mp4"
+  }, {
+    zan: 110,
+    buzan: 20,
+    zhuan: 6,
+    liuyan: 40,
+    address: "/images/shi4.mp4"
+  }
+  , {
+    zan: 110,
+    buzan: 20,
+    zhuan: 6,
+    liuyan: 40,
+    address: "/images/shi5.mp4"
+  }
+]
+
+module.exports ={
+  data :data
+}
